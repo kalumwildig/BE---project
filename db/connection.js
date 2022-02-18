a5f707d92ab171ee11a1f2a29e76b61e892abc65
Kalum Wildig <kalumwildig@Kalums-MacBook-Pro.local>
2022-02-18 14:36:40 +0000

    setup for hosting on heroku

--- db/connection.js
+++ db/connection.js
@@ -1,3 +1,4 @@
+//const { config } = require('dotenv');
 const { Pool } = require('pg');
 const ENV = process.env.NODE_ENV || 'development';
 
@@ -5,8 +6,15 @@ require('dotenv').config({
   path: `${__dirname}/../.env.${ENV}`,
 });
 
-if (!process.env.PGDATABASE) {
-  throw new Error('PGDATABASE not set');
+if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
+  throw new Error('PGDATABASE or DATABASE_URL not set');
 }
 
-module.exports = new Pool();
+const config = ENV === "production" ? {
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
+} : {}
+
+module.exports = new Pool(config);
